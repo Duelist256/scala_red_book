3604d6662eb6f7aaa5d966a87ca55e1c4feaeec4
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-04-19 23:26:51 +0300

    Add examples of using compose and methods compose and andThen of FunctionN

--- src/main/scala/chapter2/part2/Exercise2_5.scala
+++ src/main/scala/chapter2/part2/Exercise2_5.scala
@@ -4,4 +4,18 @@ object Exercise2_5 extends App {
   def compose[A, B, C](f: B => C, g: A => B): A => C = {
     a: A => f(g(a))
   }
+
+  val divideBy2AndThenSquare = compose[Int, Int, Int](x => x * x, x => x / 2)
+  println(divideBy2AndThenSquare(10))
+
+
+  // default compose and andThen example
+  val square: Int => Int = (x: Int) => x * x
+  val divideBy2: Int => Int = (x: Int) => x / 2
+
+  val sqDivBy2 = square.andThen(divideBy2)
+  val divBy2Sq = square.compose(divideBy2)
+
+  println(sqDivBy2(10))
+  println(divBy2Sq(10))
 }
