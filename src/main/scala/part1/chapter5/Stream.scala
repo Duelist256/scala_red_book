72e44a1a7cd932d915791727ac728a462321773c
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-10-02 23:09:38 +0300

    Solved 5.1-5.7

--- src/main/scala/part1/chapter5/Stream.scala
+++ src/main/scala/part1/chapter5/Stream.scala
@@ -1,21 +1,70 @@
 package part1.chapter5
 
 sealed trait Stream[+A] {
-  /* TODO Exercise 5.1
+  /* Exercise 5.1
      Write a function to convert a Stream to a List, which will force its evaluation and let
      you look at it in the REPL. You can convert to the regular List type in the standard
      library. You can place this and other functions that operate on a Stream inside the
      Stream trait. */
-  def toList: List[A] = ???
+  def toList: List[A] = this match {
+    case Empty => List.empty
+    case Cons(h, t) => h() :: t().toList
+  }
 
-  /* TODO Exercise 5.2
+
+  /* Exercise 5.2
      Write the function take(n) for returning the first n elements of a Stream, and
      drop(n) for skipping the first n elements of a Stream. */
+  def take(n: Int): Stream[A] =
+    if (n <= 0) Empty
+    else this match {
+      case Empty => Empty
+      case Cons(h, t) => Stream.cons(h(), t().take(n - 1))
+    }
 
-  /* TODO Exercise 5.3
+  /* Exercise 5.3
      Write the function takeWhile for returning all starting elements of a Stream that
      match the given predicate. */
-  def takeWhile(p: A => Boolean): Stream[A] = ???
+  def takeWhile(p: A => Boolean): Stream[A] =
+    this match {
+      case Empty => Empty
+      case Cons(h, t) =>
+        val head = h()
+        if (p(head)) Stream.cons(head, t().takeWhile(p))
+        else Empty
+    }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
+
+  /* Exercise 5.4
+     Implement forAll, which checks that all elements in the Stream match a given predicate.
+     Your implementation should terminate the traversal as soon as it encounters a
+     nonmatching value. */
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
+
+  /* Exercise 5.5
+     Use foldRight to implement takeWhile. */
+  def takeWhileFR(p: A => Boolean): Stream[A] = foldRight[Stream[A]](Empty) {
+    case (a, b) => if (p(a)) Stream.cons(a, b) else b
+  }
+
+  /* Exercise 5.6
+     Hard: Implement headOption using foldRight. */
+  def headOption(): Option[A] = foldRight[Option[A]](None)((h, _) => Some(h))
+
+  /* Exercise 5.7
+     Implement map, filter, append, and flatMap using foldRight. The append method
+     should be non-strict in its argument. */
+  def map[B](f: A => B): Stream[B] = foldRight[Stream[B]](Empty)((h, acc) => Stream.cons(f(h), acc))
+  def append[B >: A](other: => Stream[B]): Stream[B] = foldRight[Stream[B]](other)((h, acc) => Stream.cons(h, acc))
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight[Stream[B]](Empty)((a, acc) => f(a).append(acc))
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
---
+++ src/test/scala/part1/chapter5/StreamTest.scala
@@ -0,0 +1,145 @@
+package part1.chapter5
+
+import org.scalatest.{FunSpec, Matchers}
+
+class StreamTest extends FunSpec with Matchers {
+  describe("Stream test") {
+    describe("Exercise 5.1: toList") {
+      it("should convert stream of 5 elements to list") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        val list = stream.toList
+        list shouldEqual List(1, 2, 3, 4, 5)
+      }
+      it("should convert stream of 1 element to list") {
+        val stream = Stream(1)
+        val list = stream.toList
+        list shouldEqual List(1)
+      }
+      it("should convert empty stream to list") {
+        val stream = Stream.empty
+        val list = stream.toList
+        list shouldEqual List.empty
+      }
+    }
+    describe("Exercise 5.2: take") {
+      it("should take no elements from the stream") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        stream.take(-1).toList shouldEqual List()
+      }
+      it("should take no elements from the stream 2") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        stream.take(0).toList shouldEqual List()
+      }
+      it("should take first element from the stream") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        stream.take(1).toList shouldEqual List(1)
+      }
+      it("should take first 3 elements from the stream") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        stream.take(3).toList shouldEqual List(1, 2, 3)
+      }
+      it("should take first 5 elements from the stream") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        stream.take(5).toList shouldEqual List(1, 2, 3, 4, 5)
+      }
+      it("should take all elements from the stream") {
+        val stream = Stream(1, 2, 3, 4, 5)
+        stream.take(55).toList shouldEqual List(1, 2, 3, 4, 5)
+      }
+    }
+    describe("Exercise 5.3: takeWhile") {
+      it("should take no elements from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhile(_ < 0).toList shouldEqual List()
+      }
+      it("should take first element from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhile(_ == 1).toList shouldEqual List(1)
+      }
+      it("should take first 2 elements from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhile(_ % 2 == 1).toList shouldEqual List(1, 3)
+      }
+      it("should take all elements from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhile(_ > 0).toList shouldEqual List(1, 3, 4, 8)
+      }
+    }
+    describe("Exercise 5.4: forAll") {
+      it("should return true") {
+        val oddNumbers = Stream(1, 3, 5, 7)
+        oddNumbers.forAll(_ % 2 == 1) shouldBe true
+        val evenNumbers = Stream(0, 2, 4, 6, 8)
+        evenNumbers.forAll(_ % 2 == 0) shouldBe true
+      }
+      it("should return false") {
+        val stream = Stream(1, 2, 3, 5, 7)
+        stream.forAll(_ % 2 == 1) shouldBe false
+        val stream2 = Stream(0, 2, 4, 6, 7, 8)
+        stream2.forAll(_ % 2 == 0) shouldBe false
+      }
+    }
+    describe("Exercise 5.5: takeWhileFR") {
+      it("should take no elements from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhileFR(_ < 0).toList shouldEqual List()
+      }
+      it("should take first element from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhileFR(_ == 1).toList shouldEqual List(1)
+      }
+      it("should take first 2 elements from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhileFR(_ % 2 == 1).toList shouldEqual List(1, 3)
+      }
+      it("should take all elements from the stream") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.takeWhileFR(_ > 0).toList shouldEqual List(1, 3, 4, 8)
+      }
+    }
+    describe("Exercise 5.6: headOption") {
+      it("should return Some of the first element") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.headOption() shouldEqual Some(1)
+      }
+      it("should return None") {
+        val stream = Stream()
+        stream.headOption() shouldEqual None
+      }
+    }
+    describe("Exercise 5.7: map") {
+      it("should multiply each element by 2") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.map(_ * 2).toList shouldEqual List(2, 6, 8, 16)
+      }
+      it("should return empty stream") {
+        val stream = Stream[Int]()
+        stream.map(_ * 2).toList shouldEqual List()
+      }
+    }
+    describe("Exercise 5.7: append") {
+      it("should return list of 6 elements") {
+        val stream = Stream(1, 2, 3)
+        val stream2 = Stream(7, 8, 9)
+        stream.append(stream2).toList shouldEqual List(1, 2, 3, 7, 8, 9)
+        stream2.append(stream).toList shouldEqual List(7, 8, 9, 1, 2, 3)
+      }
+      it("should return empty stream") {
+        val stream = Stream(1, 2, 3)
+        val stream2 = Stream()
+        stream.append(stream2).toList shouldEqual stream.toList
+        stream2.append(stream).toList shouldEqual stream.toList
+      }
+    }
+    describe("Exercise 5.7: flatMap") {
+      it("should multiply each element by 2") {
+        val stream = Stream(1, 3, 4, 8)
+        stream.flatMap(v => Stream(v * 2)).toList shouldEqual List(2, 6, 8, 16)
+      }
+      it("should return empty stream") {
+        val stream = Stream[Int]()
+        stream.flatMap(v => Stream(v * 2)).toList shouldEqual List()
+      }
+    }
+  }
+}
