528c8ed863191f7974bfc91155952733ebb9801d
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-10-14 11:35:39 +0300

    Added 6.11

--- src/main/scala/part1/chapter6/State.scala
+++ src/main/scala/part1/chapter6/State.scala
@@ -28,4 +28,12 @@ object State {
       case Nil => unit[S, List[A]](List())
       case x :: xs => x.map2(sequence(xs))(_ :: _)
     }
+
+  def get[S]: State[S, S] = State(s => (s, s))
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
--- src/main/scala/part1/chapter6/package.scala
+++ src/main/scala/part1/chapter6/package.scala
@@ -2,4 +2,25 @@ package part1
 
 package object chapter6 {
   type Rand[+A] = RNG => (A, RNG)
+
+  /* TODO EXERCISE 6.11
+     Hard: To gain experience with the use of State, implement a finite state automaton
+     that models a simple candy dispenser. The machine has two types of input: you can
+     insert a coin, or you can turn the knob to dispense candy. It can be in one of two
+     states: locked or unlocked. It also tracks how many candies are left and how many
+     coins it contains. */
+  sealed trait Input
+  case object Coin extends Input
+  case object Turn extends Input
+  case class Machine(locked: Boolean, candies: Int, coins: Int)
+  /* The rules of the machine are as follows:
+     - Inserting a coin into a locked machine will cause it to unlock if there’s any
+       candy left.
+     - Turning the knob on an unlocked machine will cause it to dispense candy and
+       become locked.
+     - Turning the knob on a locked machine or inserting a coin into an unlocked
+       machine does nothing.
+     - A machine that’s out of candy ignores all inputs. */
+
+  def takeInput(input: Input, state: State[Machine, Int]): State[Machine, Int] = ???
 }
