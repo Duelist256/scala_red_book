48189355e0ba5b452b3fc1de2a29eb0e1af636da
Duelist256 <selykov.iw@yandex.ru>
2018-11-10 15:58:07 +0300

    Added tests for 8.6

--- src/test/scala/part1/chapter8/GenTest.scala
+++ src/test/scala/part1/chapter8/GenTest.scala
@@ -11,6 +11,7 @@ class GenTest extends FunSpec with Matchers {
         val initialRng: RNG = SimpleRNG(initialValue)
         val start = 1
         val stopExclusive = 10
+
         val (result, _) = Gen.choose(start, stopExclusive).sample.run(initialRng)
         (result >= start && result < stopExclusive) shouldBe true
       }
@@ -20,6 +21,7 @@ class GenTest extends FunSpec with Matchers {
         val initialValue = 1
         val initialRng: RNG = SimpleRNG(initialValue)
         val n = 10
+
         val (result, _) = Gen.unit(n).sample.run(initialRng)
         result shouldEqual n
       }
@@ -31,7 +33,35 @@ class GenTest extends FunSpec with Matchers {
         val size = 10
         val value = 5
         val gen = Gen.unit(value)
+
         val (result, _) = Gen.listOfN(size, gen).sample.run(initialRng)
+        result.size shouldEqual size
+        result.forall(_ == value)
+      }
+    }
+    describe("Exercise 8.6: flatMap") {
+      it("should return gen of changed value") {
+        val initialValue = 1
+        val initialRng: RNG = SimpleRNG(initialValue)
+        val value = 5
+        val n = 10
+        val expectedResult = value + n
+        val gen = Gen.unit(value)
+
+        val (result, _) = gen.flatMap(i => Gen.unit(i + n)).sample.run(initialRng)
+        result shouldEqual expectedResult
+      }
+    }
+    describe("Exercise 8.6: listOfN") {
+      it("should return list of specified value of gen") {
+        val initialValue = 1
+        val initialRng: RNG = SimpleRNG(initialValue)
+        val size = 10
+        val genSize = Gen.unit(size)
+        val value = 5
+        val genValue = Gen.unit(value)
+
+        val (result, _) = genValue.listOfN(genSize).sample.run(initialRng)
         result.size shouldEqual size
         result.forall(_ == value)
       }
