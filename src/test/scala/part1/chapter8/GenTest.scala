27aaf941f7fc1105ce39a8afd6ef9480a0453a77
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-11-10 14:54:22 +0300

    Add some tests for 8.4-8.5

---
+++ src/test/scala/part1/chapter8/GenTest.scala
@@ -0,0 +1,40 @@
+package part1.chapter8
+
+import org.scalatest.{FunSpec, Matchers}
+import part1.chapter6.{RNG, SimpleRNG}
+
+class GenTest extends FunSpec with Matchers {
+  describe("GenTest") {
+    describe("Exercise 8.4: choose") {
+      it("should return random int from 1 to 9") {
+        val initialValue = 1
+        val initialRng: RNG = SimpleRNG(initialValue)
+        val start = 1
+        val stopExclusive = 10
+        val (result, _) = Gen.choose(start, stopExclusive).sample.run(initialRng)
+        (result >= start && result < stopExclusive) shouldBe true
+      }
+    }
+    describe("Exercise 8.5: unit") {
+      it("should return gen with specified value") {
+        val initialValue = 1
+        val initialRng: RNG = SimpleRNG(initialValue)
+        val n = 10
+        val (result, _) = Gen.unit(n).sample.run(initialRng)
+        result shouldEqual n
+      }
+    }
+    describe("Exercise 8.5: listOfN") {
+      it("should return list of specified value of gen") {
+        val initialValue = 1
+        val initialRng: RNG = SimpleRNG(initialValue)
+        val size = 10
+        val value = 5
+        val gen = Gen.unit(value)
+        val (result, _) = Gen.listOfN(size, gen).sample.run(initialRng)
+        result.size shouldEqual size
+        result.forall(_ == value)
+      }
+    }
+  }
+}
