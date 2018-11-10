1f918246e2c182e2bf04aa9195c3a3562dcfd839
Duelist256 <selykov.iw@yandex.ru>
2018-11-10 17:49:41 +0300

    Implement zip for Stream

--- src/main/scala/part1/chapter5/Stream.scala
+++ src/main/scala/part1/chapter5/Stream.scala
@@ -92,6 +92,9 @@ sealed trait Stream[+A] {
     case _ => None
   }
 
+  def zip[B](s2: Stream[B]): Stream[(A,B)] =
+    zipWith(s2)((_,_))
+
   def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = Stream.unfold((this, s2)) {
     case (Empty, Empty) => None
     case (Empty, Cons(h2, t2)) => Some(((None, Some(h2())), (Empty, t2())))
