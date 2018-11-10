78413d880ca14762655dbb62b7c301ddb8241626
Duelist256 <selykov.iw@yandex.ru>
2018-11-10 18:42:50 +0300

    Added forAll

--- src/main/scala/part1/chapter8/Prop.scala
+++ src/main/scala/part1/chapter8/Prop.scala
@@ -1,9 +1,10 @@
 package part1.chapter8
 
 import part1.chapter6.RNG
-import part1.chapter8.Prop.TestCases
+import part1.chapter8.Prop.{TestCases, MaxSize}
+import part1.chapter5.Stream
 
-case class Prop(run: (TestCases, RNG) => Result) {
+case class Prop(run: (MaxSize,TestCases,RNG) => Result) {
 
   /* Exercise 8.9
      Now that we have a representation of Prop, implement && and || for composing Prop
@@ -11,8 +12,8 @@ case class Prop(run: (TestCases, RNG) => Result) {
      the left or the right. Can you devise a way of handling this, perhaps by allowing
      Prop values to be assigned a tag or label which gets displayed in the event of a failure?*/
 
-  def ||(p: Prop): Prop = Prop { (n, rng) =>
-    (this.run(n, rng), p.run(n, rng)) match {
+  def ||(p: Prop): Prop = Prop { (max, n, rng) =>
+    (this.run(max, n, rng), p.run(max, n, rng)) match {
       case (Passed, _) => Passed
       case (_, Passed) => Passed
       case (Falsified(failure, successes), Falsified(failure2, successes2)) =>
@@ -20,8 +21,8 @@ case class Prop(run: (TestCases, RNG) => Result) {
     }
   }
 
-  def &&(p: Prop): Prop = Prop { (n, rng) =>
-    (this.run(n, rng), p.run(n, rng)) match {
+  def &&(p: Prop): Prop = Prop { (max, n, rng) =>
+    (this.run(max, n, rng), p.run(max, n, rng)) match {
       case (Passed, Passed) => Passed
       case (f: Falsified, Passed) => f
       case (Passed, f: Falsified) => f
@@ -36,4 +37,39 @@ object Prop {
   type FailedCase = String
   type SuccessCount = Int
   type TestCases = Int
+  type MaxSize = Int
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (max, n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) =>
+        try {
+          if (f(a)) Passed else Falsified(a.toString, i)
+        } catch {
+          case e: Exception => Falsified(buildMsg(a, e), i)
+        }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
+    forAll(g.forSize(_))(f)
+
+  def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+    (max,n,rng) =>
+      val casesPerSize = (n + (max - 1)) / max
+      val props: Stream[Prop] =
+        Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop =
+        props.map(p => Prop { (max, _, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduce(_ && _)
+      prop.run(max,n,rng)
+  }
 }
--- src/main/scala/part1/chapter8/SGen.scala
+++ src/main/scala/part1/chapter8/SGen.scala
@@ -1,3 +1,5 @@
 package part1.chapter8
 
-case class SGen[A](forSize: Int => Gen[A])
+case class SGen[A](forSize: Int => Gen[A]) {
+  def apply(n: Int): Gen[A] = forSize(n)
+}
