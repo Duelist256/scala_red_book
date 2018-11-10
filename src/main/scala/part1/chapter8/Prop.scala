1e00f15eff6b3efd4a0db112259b693507d97eaa
Duelist256 <selykov.iw@yandex.ru>
2018-11-10 17:50:37 +0300

    Added Result, solved 8.8

--- src/main/scala/part1/chapter8/Prop.scala
+++ src/main/scala/part1/chapter8/Prop.scala
@@ -1,14 +1,39 @@
 package part1.chapter8
 
-trait Prop {
-  def check: Boolean
+import part1.chapter6.RNG
+import part1.chapter8.Prop.TestCases
 
-  /* Exercise 8.3
-     Assuming the following representation of Prop, implement && as a method of Prop. */
-  def &&(p: Prop): Boolean = check && p.check
+case class Prop(run: (TestCases, RNG) => Result) {
+
+  /* Exercise 8.9
+     Now that we have a representation of Prop, implement && and || for composing Prop
+     values. Notice that in the case of failure we don’t know which property was responsible,
+     the left or the right. Can you devise a way of handling this, perhaps by allowing
+     Prop values to be assigned a tag or label which gets displayed in the event of a failure?*/
+
+  def ||(p: Prop): Prop = Prop { (n, rng) =>
+    (this.run(n, rng), p.run(n, rng)) match {
+      case (Passed, _) => Passed
+      case (_, Passed) => Passed
+      case (Falsified(failure, successes), Falsified(failure2, successes2)) =>
+        Falsified(s"$failure/$failure2", successes + successes2)
+    }
+  }
+
+  def &&(p: Prop): Prop = Prop { (n, rng) =>
+    (this.run(n, rng), p.run(n, rng)) match {
+      case (Passed, Passed) => Passed
+      case (f: Falsified, Passed) => f
+      case (Passed, f: Falsified) => f
+      case (Falsified(failure, successes), Falsified(failure2, successes2)) =>
+        Falsified(s"$failure/$failure2", successes + successes2)
+    }
+  }
+
 }
 
 object Prop {
   type FailedCase = String
   type SuccessCount = Int
+  type TestCases = Int
 }
---
+++ src/main/scala/part1/chapter8/Result.scala
@@ -0,0 +1,15 @@
+package part1.chapter8
+
+import part1.chapter8.Prop.{FailedCase, SuccessCount}
+
+sealed trait Result {
+  def isFalsified: Boolean
+}
+
+case object Passed extends Result {
+  override def isFalsified: Boolean = false
+}
+
+case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+  override def isFalsified: Boolean = true
+}
