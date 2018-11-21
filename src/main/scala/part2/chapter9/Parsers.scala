3c69d735802156ec9a0da9bb0b7acb0185a85997
Duelist256 <selykov.iw@yandex.ru>
2018-11-22 00:08:02 +0300

    Implemented many, make arguments in some methods non-strict

--- src/main/scala/part2/chapter9/Parsers.scala
+++ src/main/scala/part2/chapter9/Parsers.scala
@@ -5,7 +5,7 @@ import part2.chapter8.{Gen, Prop}
 trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
   def char(c: Char): Parser[Char] = string(c.toString).map(_.head)
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
 
   /* Exercise 9.4
      Hard: Using map2 and succeed, implement the listOfN combinator from earlier. */
@@ -13,24 +13,24 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     if (n <= 0) succeed(List[A]())
     else p.map2[List[A], List[A]](listOfN(n - 1, p))(_ :: _)
 
+  /* Exercise 9.3
+     Hard: Before continuing, see if you can define many in terms of or, map2, and succeed. */
+  def many[A](p: Parser[A]): Parser[List[A]] =
+    many1(p) or succeed(List())
 
-  def many[A](P: Parser[A]): Parser[List[A]]
   def succeed[A](a: A): Parser[A] = string("").map(_ => a)
   def map[A, B](a: Parser[A])(f: A => B): Parser[B]
   def slice[A](a: Parser[A]): Parser[String]
   def many1[A](p: Parser[A]): Parser[List[A]] = p.map2(many(p))(_ :: _)
-  def product[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)]
 
   /* Exercise 9.1
      Using product, implement the now-familiar combinator map2 and then use this to
      implement many1 in terms of many. Note that we could have chosen to make map2
      primitive and defined product in terms of map2 as we’ve done in previous chapters.
      The choice is up to you.*/
-  def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C): Parser[C] =
+  def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
     product(p, p2).map[C](f.tupled)
-
-  /* TODO Exercise 9.3
-     Hard: Before continuing, see if you can define many in terms of or, map2, and succeed. */
 
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
@@ -44,8 +44,8 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def map[B](f: A => B): Parser[B] = self.map(p)(f)
     def many: Parser[List[A]] = self.many(p)
     def slice: Parser[String] = self.slice(p)
-    def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
-    def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+    def **[B](p2: => Parser[B]): Parser[(A, B)] = self.product(p, p2)
+    def map2[B, C](p2: => Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
   }
 
   object Laws {
