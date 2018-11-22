436a5980738d709fecfa2322defe8737ae2b6bb2
Duelist256 <selykov.iw@yandex.ru>
2018-11-22 22:59:18 +0300

    Added 9.6, solved 9.7, 9.8

--- src/main/scala/part2/chapter9/Parsers.scala
+++ src/main/scala/part2/chapter9/Parsers.scala
@@ -2,6 +2,8 @@ package part2.chapter9
 
 import part2.chapter8.{Gen, Prop}
 
+import scala.util.matching.Regex
+
 trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
   def char(c: Char): Parser[Char] = string(c.toString).map(_.head)
@@ -19,10 +21,13 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     many1(p) or succeed(List())
 
   def succeed[A](a: A): Parser[A] = string("").map(_ => a)
-  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+
+  /* Exercise 9.8
+     map is no longer primitive. Express it in terms of flatMap and/or other combinators.*/
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] = p.flatMap(a => succeed(f(a)))
   def slice[A](a: Parser[A]): Parser[String]
   def many1[A](p: Parser[A]): Parser[List[A]] = p.map2(many(p))(_ :: _)
-  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] = p.flatMap(a => p2.flatMap(b => succeed((a, b))))
 
   /* Exercise 9.1
      Using product, implement the now-familiar combinator map2 and then use this to
@@ -32,10 +37,23 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   def map2[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
     product(p, p2).map[C](f.tupled)
 
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  /* Exercise 9.7
+     Implement product and map2 in terms of flatMap.*/
+  def map2FM[A,B,C](p: Parser[A], p2: => Parser[B])(f: (A,B) => C): Parser[C] =
+    p.flatMap(a => p2.flatMap(b => succeed(f(a, b))))
+
+  /* TODO Exercise 9.6
+     Using flatMap and any other combinators, write the context-sensitive parser we
+     couldn’t express earlier. To parse the digits, you can make use of a new primitive,
+     regex, which promotes a regular expression to a Parser. In Scala, a string s can
+     be promoted to a Regex object (which has methods for matching) using s.r, for
+     instance, "[a-zA-Z_][a-zA-Z0-9_]*".r.*/
+  implicit def regex(r: Regex): Parser[String]
   implicit def string(s: String): Parser[String]
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
-  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] =
-    ParserOps(f(a))
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
 
 
   case class ParserOps[A](p: Parser[A]) {
@@ -46,6 +64,7 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def slice: Parser[String] = self.slice(p)
     def **[B](p2: => Parser[B]): Parser[(A, B)] = self.product(p, p2)
     def map2[B, C](p2: => Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {
