7fe1b432b3bc8dd2128192d358d704cd601026e9
Duelist256 <selykov.iw@yandex.ru>
2018-11-27 10:01:40 +0300

    Added 10.12-10.14, solved 10.15

---
+++ src/main/scala/part3/chapter10/Foldable.scala
@@ -0,0 +1,56 @@
+package part3.chapter10
+
+import part1.chapter3.Tree
+import part1.chapter5.Stream
+
+trait Foldable[F[_]] {
+  def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
+  def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
+  def foldMap[A, B](as: F[A])(f: A => B)(m: Monoid[B]): B
+  def concatenate[A](as: F[A])(m: Monoid[A]): A =
+    foldLeft(as)(m.zero)(m.op)
+
+  /* Exercise 10.15
+     Any Foldable structure can be turned into a List. Write this conversion in a generic way:*/
+  def toList[A](fa: F[A]): List[A] = foldRight(fa)(List[A]())(_ :: _)
+}
+
+object Foldable {
+  /* TODO Exercise 10.12
+     Implement Foldable[List], Foldable[IndexedSeq], and Foldable[Stream].
+     Remember that foldRight, foldLeft, and foldMap can all be implemented in terms
+     of each other, but that might not be the most efficient implementation.*/
+  val listFoldable = new Foldable[List] {
+    override def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = ???
+    override def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B = ???
+    override def foldMap[A, B](as: List[A])(f: A => B)(m: Monoid[B]): B = ???
+  }
+
+  val indexedSeqFoldable = new Foldable[IndexedSeq] {
+    override def foldRight[A, B](as: IndexedSeq[A])(z: B)(f: (A, B) => B): B = ???
+    override def foldLeft[A, B](as: IndexedSeq[A])(z: B)(f: (B, A) => B): B = ???
+    override def foldMap[A, B](as: IndexedSeq[A])(f: A => B)(m: Monoid[B]): B = ???
+  }
+
+  val streamFoldable = new Foldable[Stream] {
+    override def foldRight[A, B](as: Stream[A])(z: B)(f: (A, B) => B): B = ???
+    override def foldLeft[A, B](as: Stream[A])(z: B)(f: (B, A) => B): B = ???
+    override def foldMap[A, B](as: Stream[A])(f: A => B)(m: Monoid[B]): B = ???
+  }
+
+  /* TODO Exercise 10.13
+     Recall the binary Tree data type from chapter 3. Implement a Foldable instance for it. */
+  val treeFoldable = new Foldable[Tree] {
+    override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B): B = ???
+    override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B = ???
+    override def foldMap[A, B](as: Tree[A])(f: A => B)(m: Monoid[B]): B = ???
+  }
+
+  /* TODO Exercise 10.14
+     Write a Foldable[Option] instance.*/
+  val optionFoldable = new Foldable[Option] {
+    override def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B): B = ???
+    override def foldLeft[A, B](as: Option[A])(z: B)(f: (B, A) => B): B = ???
+    override def foldMap[A, B](as: Option[A])(f: A => B)(m: Monoid[B]): B = ???
+  }
+}
