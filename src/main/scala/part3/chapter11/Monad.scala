e8193d40febf0cd0135261079874b6ec2e382e55
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-12-01 17:43:04 +0300

    Solved 11.1, added 11.2

--- src/main/scala/part3/chapter11/Monad.scala
+++ src/main/scala/part3/chapter11/Monad.scala
@@ -1,5 +1,7 @@
 package part3.chapter11
 
+import part1.chapter5.{Cons, Empty, Stream}
+import part2.chapter7.Par
 import part2.chapter8.Gen
 
 trait Monad[F[_]] extends Functor[F] {
@@ -17,4 +19,35 @@ object Monad {
     override def unit[A](a: => A): Gen[A] = Gen.unit(a)
     override def flatMap[A, B](fa: Gen[A])(f: A => Gen[B]): Gen[B] = fa flatMap f
   }
+
+  /* Exercise 11.1
+     Write monad instances for Par, Parser, Option, Stream, and List.*/
+  val parMonad = new Monad[Par] {
+    override def unit[A](a: => A): Par[A] = Par.unit(a)
+    override def flatMap[A, B](fa: Par[A])(f: A => Par[B]): Par[B] = Par.flatMap(fa)(f)
+  }
+
+  val parserMonad = ???
+
+
+  val optionMonad = new Monad[Option] {
+    override def unit[A](a: => A): Option[A] = Some(a)
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa flatMap f
+  }
+
+  val streamMonad = new Monad[Stream] {
+    override def unit[A](a: => A): Stream[A] = Cons(() => a, () => Empty)
+    override def flatMap[A, B](fa: Stream[A])(f: A => Stream[B]): Stream[B] = fa flatMap f
+  }
+
+  val listMonad = new Monad[List] {
+    override def unit[A](a: => A): List[A] = List(a)
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa flatMap f
+  }
+
+  /* TODO Exercise 11.2
+     Hard: State looks like it would be a monad too, but it takes two type arguments and
+     you need a type constructor of one argument to implement Monad. Try to implement a
+     State monad, see what issues you run into, and think about possible solutions. We’ll
+     discuss the solution later in this chapter. */
 }
