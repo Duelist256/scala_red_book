3cddc23fb5330a80c99aa94471d937870d3488b1
Duelist256 <selykov.iw@yandex.ru>
2018-12-04 23:42:12 +0300

    Solved 12.3

--- src/main/scala/part3/chapter12/Applicative2.scala
+++ src/main/scala/part3/chapter12/Applicative2.scala
@@ -14,4 +14,13 @@ trait Applicative2[F[_]] extends Functor[F] {
   def map[A,B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] =
     apply(apply(unit(f.curried))(fa))(fb)
+
+  /* Exercise 12.3
+     The apply method is useful for implementing map3, map4, and so on, and the pattern
+     is straightforward. Implement map3 and map4 using only unit, apply, and the curried
+     method available on functions. */
+  def map3[A,B,C,D](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => D): F[D] =
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+  def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E): F[E] =
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
 }
