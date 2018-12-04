36e1876cf8bb7d529bee9a99fdf575bc5d940e17
Duelist256 <selykov.iw@yandex.ru>
2018-12-04 23:31:01 +0300

    Solved 12.2

---
+++ src/main/scala/part3/chapter12/Applicative2.scala
@@ -0,0 +1,17 @@
+package part3.chapter12
+
+import part3.chapter11.Functor
+
+/* Exercise 12.2
+   Hard: The name applicative comes from the fact that we can formulate the Applicative
+   interface using an alternate set of primitives, unit and the function apply, rather than
+   unit and map2. Show that this formulation is equivalent in expressiveness by defining
+   map2 and map in terms of unit and apply. Also establish that apply can be implemented
+   in terms of map2 and unit. */
+trait Applicative2[F[_]] extends Functor[F] {
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = map2(fa, fab)((a, f) => f(a))
+  def unit[A](a: => A): F[A]
+  def map[A,B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A,B) => C): F[C] =
+    apply(apply(unit(f.curried))(fa))(fb)
+}
